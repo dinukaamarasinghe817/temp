8be4950bbe70e0c322f08d9a954e589a1b5096c9
dinuka <dinukaamarasinghe817@gmail.com>
2022-11-14 07:40:53 +0530

    just 1

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,5 +1,11 @@
 <?php
 
 class Login extends Controller{
-    
+    public function student($username = '', $password = ''){
+        $user = $this->model('User');
+        $user->username = $name;
+        $user->password = $password;
+        
+        $this->view('home/index', ['name'=> $user->name, 'password'=> $user->password]);
+    }
 }
